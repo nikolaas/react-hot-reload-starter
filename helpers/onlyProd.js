79a0b200faaef7b2a9d4885ccc2f1a0c9cd384d9
Nikolay Stupak <nikolaystupak@gmail.com>
2018-03-17 14:53:33 +0300

    removed debug logging

--- helpers/onlyProd.js
+++ helpers/onlyProd.js
@@ -1,7 +1,6 @@
 module.exports = function onlyProd(element) {
     if (process.env.NODE_ENV === 'production') {
-        console.log('Only in prod', element);
         return [element];
     }
     return [];
-}
+};
