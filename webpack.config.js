57db68c3011bbe78d79ee9bc03f3512aa2af906d
Nikolay Stupak <nstupak@luxoft.com>
2018-03-19 13:27:28 +0300

    disabled source maps for vendors css

--- webpack.config.js
+++ webpack.config.js
@@ -78,7 +78,7 @@ module.exports = {
                     {
                         loader: 'style-loader',
                         options: {
-                            sourceMap: true,
+                            sourceMap: false,
                             hmr: false,
                         }
                     },
