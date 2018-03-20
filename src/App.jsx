4424e48c6e2b14ba719a0d19833d665b9ebb5838
Nikolay Stupak <nikolaystupak@gmail.com>
2018-03-20 22:27:12 +0300

    404 page was added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router';
 import { asyncComponent } from './components/AsyncComponent';
+import NotFound from './routes/NotFound';
 import './styles.scss';
 
 const SyncCounter = asyncComponent(() => import('./routes/SyncCounter'));
@@ -12,6 +13,7 @@ export default () => {
             <Route exact path="/" component={() => <Redirect to="/sync-counter"/>}/>
             <Route exact path="/sync-counter" component={SyncCounter}/>
             <Route exact path="/async-computer" component={AsyncComputer}/>
+            <Route component={NotFound}/>
         </Switch>
     );
 };
